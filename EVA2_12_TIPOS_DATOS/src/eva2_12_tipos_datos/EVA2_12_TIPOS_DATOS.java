12ef40e005e04539012c64aa4c277d08c80906c5
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-11-08 20:34:32 -0600

    EVA2_12_TIPOS_DATOS

---
+++ EVA2_12_TIPOS_DATOS/src/eva2_12_tipos_datos/EVA2_12_TIPOS_DATOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_12_tipos_datos;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA2_12_TIPOS_DATOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        byte valor =-128;
+        byte valor2 = 127;
+        System.out.println("Valor ="+valor);
+        System.out.println("Valor2 ="+valor2);
+        short val = -32768;
+        short val2 = 32767;
+        int x = 
+        int z =
+    }
+    
+}
