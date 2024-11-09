b682f9d178897d05abde888672dcbb273da9700b
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-11-08 20:28:23 -0600

    EVA2_10_VALIDAR_2

---
+++ EVA2_10_VALIDAR_2/src/eva2_10_validar_2/EVA2_10_VALIDAR_2.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_10_validar_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA2_10_VALIDAR_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int dia;
+        Scanner captu=new Scanner(System.in);
+        do{
+        System.out.println(" Numero correpondiente al dia (1a7)");
+        dia=captu.nextInt();
+        
+        }while(true);
+        
+       
+               
+    }
+    
+}
