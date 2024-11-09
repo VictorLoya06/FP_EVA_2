f67403712f82bed2952914a479b1e96934e113c0
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-11-08 20:36:01 -0600

    EVA2_15_ARREGLOS

---
+++ EVA2_15_ARREGLOS/src/eva2_15_arreglos/EVA2_15_ARREGLOS.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_15_arreglos;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA2_15_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String[] diassem = new String[7];
+        diassem[0] = "Domingo";
+        diassem[1] = "Lunes";
+        diassem[2] = "Martes";
+        diassem[3] = "Miercoles";
+        diassem[4] = "Jueves";
+        diassem[5] = "viernes";
+        diassem[6] = "Sabado";
+        
+         for (int i = 0; i < diassem.length; i++) { 
+        System.out.println(diassem[i]);}
+    }
+    
+}
