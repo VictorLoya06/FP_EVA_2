e96e62a8e737eed8aa783d3f9abf700c3928c3ff
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-11-08 20:35:33 -0600

    EVA2_14_CICLOS_ARREGLOS

---
+++ EVA2_14_CICLOS_ARREGLOS/src/eva2_14_ciclos_arreglos/EVA2_14_CICLOS_ARREGLOS.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_14_ciclos_arreglos;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA2_14_CICLOS_ARREGLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int dato,valores[];
+        int[]datos,x,y,z;
+        int[] arreglo = new int[30];
+        
+        for (int i = 0; i < arreglo.length; i++) {
+            arreglo[i]=(int)(Math.random()*100);}
+        
+            //System.out.println(arreglo);
+            
+        for (int i = 0; i < arreglo.length; i++) { 
+        System.out.print(arreglo[i]+"-");}
+    
+        
+    }
+    
+}
