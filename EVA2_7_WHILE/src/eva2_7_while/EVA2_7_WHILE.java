45b869928727a25c1ea387168874f3b9fb024312
VictorLoya06 <L24550312@chihuahua2.tecnm.mx>
2024-11-08 20:25:38 -0600

    EVA2_7_WHILE

---
+++ EVA2_7_WHILE/src/eva2_7_while/EVA2_7_WHILE.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_7_while;
+
+/**
+ *
+ * @author noziv
+ */
+public class EVA2_7_WHILE {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       int val = 1;
+        
+        while (val <= 10) {
+            System.out.print(val + "_");
+            val++;
+    }
+       // System.out.println("");
+        
+      //do{
+        // System.out.print(val+"_");
+        // val++;
+     // }while(val<=10); 
+      
+      }
+      }
+              
+      
+    
+    
+
